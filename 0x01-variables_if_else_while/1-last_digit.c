ea02b1ba5079c5657dc7630d85529b7b5232f638
david joy <davidjoy.win@gmail.com>
2022-09-10 22:09:09 +0100

    last digit task amendment

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-		printf("Last digit of n is %d and is greater than 5", n);
+		printf("Last digit of %d and is greater than 5", n);
 	else if (n == 0)
-		printf("Last digit of n is %d and is 0", n);
+		printf("Last digit of %d and is 0", n);
 	else
-		printf("Last digit of n is %d and is less than 6 and not 0", n);
+		printf("Last digit of %d and is less than 6 and not 0", n);
 	/* your code goes there */
 
 	return (0);

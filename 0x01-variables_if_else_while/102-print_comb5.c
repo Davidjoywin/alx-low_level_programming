24048bbc4d5e924a0e0c1b43aa4effc82921efb7
david joy <davidjoy.win@gmail.com>
2022-09-10 23:14:13 +0100

    print comb5 corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,34 @@
 int main(void)
 {
 	int l_num1 = 0;
-	int l_num2 = 0;
-	int r_num1 = 0;
-	int r_num2 = 0;
+	int l_num2;
+	int r_num1;
+	int r_num2;
 
 	while (l_num1 < 10)
 	{
+		l_num2 = 0;
 		while (l_num2 < 10)
 		{
+			r_num1 = 0;
 			while (r_num1 < 10)
 			{
+				r_num2 = 0;
 				while (r_num2 < 10)
 				{
-					putchar(48 + l_num1);
-					putchar(48 + l_num2);
-					putchar(' ');
-					putchar(48 + r_num1);
-					putchar(48 + r_num2);
-					putchar(',');
-					putchar(' ');
+					if (!(l_num1 == r_num2 && l_num2 == r_num1))
+					{
+						putchar(48 + l_num1);
+						putchar(48 + l_num2);
+						putchar(' ');
+						putchar(48 + r_num1);
+						putchar(48 + r_num2);
+						if (!(l_num1 == l_num2 && r_num1 + r_num2 == 17 && r_num1 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 					r_num2++;
 				}
 				r_num1++;

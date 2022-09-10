697f4d339fce29398c2b6bf6b97290f2acc42f98
David <davidjoy2k18@gmail.com>
2022-09-09 19:07:35 -0700

    low level programming hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - to print something to the terminal
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

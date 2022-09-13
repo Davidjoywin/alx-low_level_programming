a85e427c85c51fe5db806a91b892f643f4f7eb4d
david joy <davidjoy.win@gmail.com>
2022-09-13 17:54:40 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,14 @@
 /**
  * main - entry point
  *
- * Returns: return 0
+ * Returns: 0
  */
 
 int main(void)
 {
 	char word[] = "_putchar";
 	int i = 0;
+
 	while (i < (int)sizeof(word) - 1)
 	{
 		_putchar(word[i]);

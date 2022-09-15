423baef2a9de319e8bc54bceb747e03f6df4fb8e
david joy <davidjoy.win@gmail.com>
2022-09-15 16:17:05 +0100

    task 1

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ *
+ * Desc: Header file containing the function_nested_loop
+ * 	 header prototype
+ */
+int _putchar(char val);
+int _isupper(int c);
+
+#endif

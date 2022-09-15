4360df22e6c34018978d2232eccd788d4f5c4180
david joy <davidjoy.win@gmail.com>
2022-09-15 16:30:04 +0100

    task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+  * _ispper - checks if an alphabet is uppercase or lowercase
+  * @c: character parameter
+  * Return: return 0 or 1
+  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')

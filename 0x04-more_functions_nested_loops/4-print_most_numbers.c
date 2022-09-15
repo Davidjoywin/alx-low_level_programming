40894a696fad33d7a4e0687bde67118eac674f47
david joy <davidjoy.win@gmail.com>
2022-09-15 18:19:36 +0100

    task updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,21 @@
 
 /**
  * print_most_numbers - prints numbers from 0 - 9
- * without print 2 and 4
+ *						without print 2 and 4
  */
 
 void print_most_numbers(void)
 {
-	for (int i = 0; i < 9; i++)
+	int i;
+
+	i = -1;
+	while (i < 10)
 	{
-		if (i != 4 || i != 2)
+		i++;
+		if (i == 2 || i == 4)
 		{
-			_putchar('0' + i);
+			continue;
 		}
+		_putchar('0' + i);
 	}
+}

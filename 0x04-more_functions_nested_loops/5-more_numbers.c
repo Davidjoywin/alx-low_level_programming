21f1c0ce9e1587cf4705bdf8fb6f04be59c3e07b
david joy <davidjoy.win@gmail.com>
2022-09-15 18:33:07 +0100

    task updated

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from
+ * 					from 0 - 14, followed by new line
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 14)
+	{
+		if (i < 10)
+		{
+			_putchar('0' + i);
+		}
+		else
+		{
+			_putchar('0' + i/10);
+			_putchar('0' + i%10);
+		}
+	}
+}

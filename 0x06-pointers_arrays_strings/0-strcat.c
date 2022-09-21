dce328df87b02ef3c6a2c9c7146b88ec41fd2023
David Joy <Davidjoy.win@gmail.com>
2022-09-21 22:44:35 +0100

    task update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - returns a pointer to the returning string
+ * @dest: parameter dest
+ * @src: params src
+ * Return: return a pointer to a string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = sizeof(dest) / sizeof(char);
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);

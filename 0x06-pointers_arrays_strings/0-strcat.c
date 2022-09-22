f6359b3f1f47c418e5d1cc18e8f63d4eff4c4b53
David Joy <Davidjoy.win@gmail.com>
2022-09-22 09:52:48 +0100

    task updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 
@@ -13,8 +12,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = sizeof(dest);
+	i = 0;
 	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
 	while (src[j] != '\0')
 	{

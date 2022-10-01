2b7e211b0738a1d2a1ca320e94ce8c06476c7b93
David Joy <Davidjoy.win@gmail.com>
2022-10-01 05:44:50 +0100

    task updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
  * main - adds positive numbers and print to terminal
  * @argc: size of argv
@@ -17,21 +18,19 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (!(isdigit(argv[i][0])))
 		{
-			if (!(isdigit(argv[i][0])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				result += atoi(argv[i]);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", result);
-		return (0);
+		else
+		{
+			result += atoi(argv[i]);
+		}
 	}
+	printf("%d\n", result);
+	return (0);
 }
+

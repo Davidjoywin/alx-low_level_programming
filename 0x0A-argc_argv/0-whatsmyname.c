f37ed4bcc1771001cd040159bc491fc52b712b24
David Joy <Davidjoy.win@gmail.com>
2022-10-01 03:09:31 +0100

    task updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point. print its name
+ * @argc: size of argv
+ * @argv: arg passed in the terminal
+ * Return: return int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument passed into it
+ * @argc: size of argv
+ * @argv: array of arg passed in terminal
+ * Return: return int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", num - 1);
+	return (0);
+}
+

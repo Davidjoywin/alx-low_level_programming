38902e0930b3f6d2d7f53caf481a0ba79be55577
David Joy <Davidjoy.win@gmail.com>
2022-10-05 04:49:50 +0100

    task updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,9 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
+	if (width < 1 || height < 1)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(*(arr[i])));

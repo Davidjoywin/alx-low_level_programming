bcf7c8acfacebf979b9e15c4899f08234803ae55
David Joy <Davidjoy.win@gmail.com>
2022-10-07 05:44:53 +0100

    task updated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,10 +10,9 @@
 
 int *array_range(int min, int max)
 {
-	int range, *new_mem;
+	int *new_mem;
 
-	range = max - min
-	new_mem = malloc(char(int) *range)
+	new_mem = malloc(sizeof(int) *(max - min));
 	if (new_mem == NULL && min > max)
 		return (NULL);
 

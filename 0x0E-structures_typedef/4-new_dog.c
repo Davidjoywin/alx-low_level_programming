4ac530073a3b4fb7c904adac456d7e9663704c27
David Joy <Davidjoy.win@gmail.com>
2022-10-11 03:46:47 +0100

    task completed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *name, owner;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
+	name = name;
+	owner = owner;
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;

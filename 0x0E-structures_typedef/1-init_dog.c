e47ac74deb3afad5a61db0fd13e72609f4ed5e14
David Joy <Davidjoy.win@gmail.com>
2022-10-11 04:02:32 +0100

    task completed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -10,7 +11,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	d = malloc(sizeof(struct dog));
+	d = { name, age, owner };
 }

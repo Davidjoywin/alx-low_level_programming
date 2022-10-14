6535205340012b61882435e1e534c7edae663ae1
David Joy <Davidjoy.win@gmail.com>
2022-10-14 05:46:20 +0100

    task completed

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @seperator: Seperate the printed values
+ * @n: number of variadic args
+ */
+
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	int i;
+	char *tmp;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		tmp = va_arg(nums, char *);
+		if (tmp == NULL)
+			tmp = NULL;
+
+		printf("%s", tmp);
+		if (seperator != NULL && i < (int)n - 1)
+			printf("%s", seperator);
+	}
+	putchar('\n');
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+C - VARIADIC FUNCTIONS
+
+sum_them_all - returns the sum of all its parameters
+print_numbers - prints the numbers, followed by a new line
+print_strings - prints strings, followed by a new line
+print_all - prints anything
+
